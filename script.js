53b2122e9cd809e9da195af860cf02ce55a70ff5
matildabergstrom <matilda.bergstrom@student.tabyenskilda.se>
2023-02-02 12:59:40 +0100

    a

---
+++ script.js
@@ -0,0 +1,11 @@
+/* OVERLAY MENU */
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
+  /* OVERLAY MENU END */
